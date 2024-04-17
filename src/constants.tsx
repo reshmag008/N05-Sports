0c4a063aca7d0d815afe738d586841db528eb594
Reshma <reshma.g@iinerds.com>
2024-04-17 15:09:29 +0530

    ug fix

--- src/constants.tsx
+++ src/constants.tsx
@@ -1,2 +1,2 @@
-export const BACKEND_URL = "http://localhost:8443";  //"https://lets-cricket-be.onrender.com" ;
+export const BACKEND_URL = "https://lets-cricket-be.onrender.com" ; //"http://localhost:8443";  //
 export const TOTAL_PLAYER = 13
